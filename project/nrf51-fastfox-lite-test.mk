fb8409d46d62925e4df90f84b86c4e9004f0221a
Sheep Sun <sunxiaoyang2003@gmail.com>
2021-11-24 14:53:44 +0800

    [project][nrf51-fastfox-lite-test]Enable a bunch of features

--- project/nrf51-fastfox-lite-test.mk
+++ project/nrf51-fastfox-lite-test.mk
@@ -1,7 +1,14 @@
 MODULES += \
-	app/shell \
-
-
-
+  app/shell \
+  app/stringtests \
+  app/tests \
+  arch/test \
+  dev/gpio \
+  lib/aes \
+  lib/aes/test \
+  lib/cksum \
+  lib/debugcommands \
+  lib/unittest \
+  lib/version
 
 include project/target/nrf-fastfox-lite.mk
